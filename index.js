1ad418013d11fbf1bce50aff22694ebdd2d7b6dc
Sofia Ford <scfordw@bu.edu>
2025-09-16 10:11:33 -0400

    Creating the functions for the javasxript calculator

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ function doAdd(){
 }
 
 function doSubtract(){
-
+    output.innerHTML = String(Number(one.value) - Number(two.value));
 }
 
 function doMultiply(){
-
+    output.innerHTML = String(Number(one.value) * Number(two.value));
 }
 
 function doDivide(){
+    output.innerHTML = String(Number(one.value) / Number(two.value))
+}
 
+function doPower(){
+    let total = Number(one.value);
+
+    for (let i = 1; i <= (Number(two.value)-1); i++){
+        total *= Number(one.value);
+    }
+
+    output.innerHTML = String(Number(total));
+}
+
+function doClear(){
+    one.innerHTML = "";
+    two.innerHTML = "";
+    output.innerHTML = "";
 }
