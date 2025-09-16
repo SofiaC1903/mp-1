d4e56da38420429d3d34be215fa43e4d4ad84a47
Sofia Ford <scfordw@bu.edu>
2025-09-16 01:06:07 -0400

    Fixing css error temporarily and starting on js calculator

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ let output = document.getElementById("output");
 function doAdd(){
     output.innerHTML = String(Number(one.value) + Number(two.value));
 }
+
+function doSubtract(){
+
+}
+
+function doMultiply(){
+
+}
+
+function doDivide(){
+
+}
